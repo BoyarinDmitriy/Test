871d5d8cd83fd0d6e7a6b4c0fac704dfbe529eb3
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-05-25 19:35:00 +0300

    Task9

--- Task9/src/task9/Main.java
+++ Task9/src/task9/Main.java
@@ -1,8 +1,32 @@
 package task9;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        int n = 0;
+
+        System.out.print("Enter the length of the sequence: ");
+        try{
+            n = in.nextInt();
+            if(n < 0)
+                throw new InputMismatchException();
+        }catch (InputMismatchException e){
+            System.out.println("Incorrect input!");
+            System.exit(0);
+        }
+
+        int sum = 0;
+        for(int i = 0; i <= n; i++){
+            if(i % 2 == 0){
+                System.out.print(i + " ");
+                sum += i;
+            }
+        }
+
+        System.out.printf("\nThe sum of the even numbers from 0 to %d: " + sum, n);
     }
 }
