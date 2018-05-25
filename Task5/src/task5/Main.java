3e92d0669925f82997f5ea2fed616b6a47736cbd
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-05-25 17:24:30 +0300

    Task5

--- Task5/src/task5/Main.java
+++ Task5/src/task5/Main.java
@@ -1,8 +1,34 @@
 package task5;
+import java.util.Scanner;
 
 public class Main {
 
+    public static int wordsCount(String text, String word){
+        int wordsCount = 0;
+
+        text = text.replaceAll( "\\p{Punct}|^|$", " ").toLowerCase();
+
+        String pattern = " " + word.toLowerCase() + " ";
+
+        while (text.contains(pattern)){
+            text = text.replaceFirst(pattern, " ");
+            wordsCount ++;
+        }
+
+        return  wordsCount;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the text: ");
+        String text = in.nextLine();
+
+        System.out.print("Enter the word: ");
+        String word = in.nextLine();
+
+        int wordsCount = wordsCount(text, word);
+
+        System.out.println("Count: " + wordsCount);
     }
 }
