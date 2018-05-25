826d5920a2f76a8357b5d866e8e93b6353829e4f
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-05-25 14:47:37 +0300

    Task4

--- Task4/src/task4/Main.java
+++ Task4/src/task4/Main.java
@@ -1,8 +1,28 @@
 package task4;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	    
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the sentence: ");
+        String sentence = in.nextLine();
+
+        String[] sentenceSplit = sentence.split(" |,");
+        Arrays.sort(sentenceSplit);
+
+        System.out.print("Words: ");
+        int wordsCount = 0;
+
+        for(int i = 0; i < sentenceSplit.length; i++) {
+            if(!sentenceSplit[i].isEmpty()) {
+                System.out.print(sentenceSplit[i] + " ");
+                wordsCount++;
+            }
+        }
+
+        System.out.println("\nCount: " + wordsCount);
     }
 }
