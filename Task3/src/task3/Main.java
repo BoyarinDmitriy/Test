53a365f27750a75e3e1f364a6b064614132bdc4f
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-05-25 13:42:24 +0300

    Task3

--- Task3/src/task3/Main.java
+++ Task3/src/task3/Main.java
@@ -1,8 +1,26 @@
 package task3;
+import java.util.Scanner;
 
 public class Main {
 
+    public static boolean isPalindrome(String str){
+        boolean isPalindrome = true;
+
+        for(int i = 0; i < str.length() / 2; i++){
+            if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        return  isPalindrome;
+    }
+
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the string: ");
+        String str = in.nextLine();
+        System.out.println(isPalindrome(str));
     }
 }
