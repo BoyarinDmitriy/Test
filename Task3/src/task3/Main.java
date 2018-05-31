056496d16bdf899f10e5b35c9ea73fd659f18511
Dmitriy <boyarin.dmitriy1998@gmail.com>
2018-05-31 14:01:09 +0300

    Task3

--- Task3/src/task3/Main.java
+++ Task3/src/task3/Main.java
@@ -3,24 +3,28 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static boolean isPalindrome(String str){
-        boolean isPalindrome = true;
+//    public static boolean isPalindrome(StringBuilder str){
+//        boolean isPalindrome = true;
+//
+//       for(int i = 0; i < str.length() / 2; i++){
+//            if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+//                isPalindrome = false;
+//                break;
+//            }
+//        }
+//
+//        return  isPalindrome;
+//    }
 
-        for(int i = 0; i < str.length() / 2; i++){
-            if(str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        return  isPalindrome;
+    public static boolean isPalindrome(StringBuilder string){
+        return string.toString().equals(string.reverse().toString());
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Enter the string: ");
-        String str = in.nextLine();
+        StringBuilder str = new StringBuilder(in.nextLine());
 
         System.out.println(isPalindrome(str));
     }
